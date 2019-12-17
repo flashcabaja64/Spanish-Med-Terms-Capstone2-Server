9a9ee551ec1c030645f6024c19d0ad74b02e45c7
Hubert Yang <offspring7890@yahoo.com>
2019-12-17 13:49:57 -0800

    removed CLIENT_ORIGIN

--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
   DATABASE_URL: process.env.DATABASE_URL
     || 'postgresql://postgres@localhost/spanishmedterms',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
