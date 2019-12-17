f1443df9af0472a8e320905270ea064d0f4f172a
Hubert Yang <offspring7890@yahoo.com>
2019-12-17 10:45:42 -0800

    changed package.json

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
+  DATABASE_URL: process.env.DATABASE_URL
     || 'postgresql://postgres@localhost/spanishmedterms',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
