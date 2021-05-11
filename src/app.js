60eb250b93c9100a743f07b6578baa00f4722979
Hubert Yang <offspring7890@yahoo.com>
2021-05-11 09:53:46 -0700

    cors

--- src/app.js
+++ src/app.js
@@ -13,18 +13,11 @@ const corsOptions = {
   origin: '*',
   optionsSuccessStatus: 200 
 }
+app.use(cors(corsOptions));
 app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
   skip: () => NODE_ENV === 'test' }));
-//app.use(cors(corsOptions));
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://hubert-spanishmedterms.vercel.app/api");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
 app.use(helmet());
+
 
 app.use('/api/auth', authRouter);
 app.use('/api/language', languageRouter);
