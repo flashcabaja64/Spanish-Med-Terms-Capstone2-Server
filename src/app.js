72ae4842bf793f59ffbfd1c3df2e518b152b5756
Hubert Yang <offspring7890@yahoo.com>
2021-05-16 23:24:26 -0700

    updated packages, and config files

--- src/app.js
+++ src/app.js
@@ -9,31 +9,19 @@ const languageRouter = require('./language/language-router');
 const userRouter = require('./user/user-router');
 
 const app = express();
-const corsOptions = {
-  origin: '*',
-  optionsSuccessStatus: 200 
-}
-app.use(cors(corsOptions));
-app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
-  skip: () => NODE_ENV === 'test' }));
-app.use(helmet());
 
+app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
+  skip: () => NODE_ENV === 'test' 
+  })
+);
+
+app.use(helmet());
+app.use(cors());
 
 app.use('/api/auth', authRouter);
 app.use('/api/language', languageRouter);
 app.use('/api/user', userRouter);
 
 app.use(errorHandler);
-/*
-app.use(function errorHandler(error, req, res, next) {
-  let response;
-  if (NODE_ENV === 'production') {
-    response = { error: { message: 'server error' } };
-  } else {
-    console.error(error);
-    response = { message: error.message, error };
-  }
-  res.status(500).json(response);
-});
-*/
+
 module.exports = app;
