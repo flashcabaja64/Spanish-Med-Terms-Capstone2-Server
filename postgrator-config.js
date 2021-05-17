322290fd32172971a4ff58df5d500da4a11c9f2b
Hubert Yang <offspring7890@yahoo.com>
2021-05-16 23:14:08 -0700

    v2 eidt config

--- postgrator-config.js
+++ postgrator-config.js
@@ -5,8 +5,11 @@ pg.defaults.ssl = process.env.NODE_ENV === "production";
 module.exports = {
   migrationDirectory: 'migrations',
   driver: 'pg',
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+    /*
   connectionString: (process.env.NODE_ENV === 'test')
     ? process.env.TEST_DATABASE_URL 
     : process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
+    */
 };
